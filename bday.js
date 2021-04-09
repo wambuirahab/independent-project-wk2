c9a17c8542d37c0d689b7e1c5c48ff799d5edf0b
wambuirahab <sarahngaruiya2000@gmail.com>
2021-04-09 17:04:09 +0300

    return day of weeK

--- bday.js
+++ bday.js
@@ -11,4 +11,28 @@ function getDate() {
 function getDayofWeek(CC, YY, MM, DD) {
   var _day = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
   day = Math.trunc(parseInt(_day));
+
+  if (day == 0) {
+    day_of_week = "Sunday";
+  }
+  if (day == 1) {
+    day_of_week = "Monday";
+  }
+  if (day == 2) {
+    day_of_week = "Tuesday";
+  }
+  if (day == 3) {
+    day_of_week = "Wednesday";
+  }
+  if (day == 4) {
+    day_of_week = "Thursday";
+  }
+  if (day == 5) {
+    day_of_week = "Friday";
+  }
+  if (day == 6) {
+    day_of_week = "Saturday";
+  }
+
+  return day_of_week
 }
