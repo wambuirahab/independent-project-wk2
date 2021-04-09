b7ceaeeed5ff7224fbe13fe359a3618453a1685f
wambuirahab <sarahngaruiya2000@gmail.com>
2021-04-09 16:55:11 +0300

    get century digits and yy

--- bday.js
+++ bday.js
@@ -3,4 +3,7 @@ function getDate() {
   var gender;
   date_value = document.getElementById("txt_bday").value;
   gender = document.getElementById("select_gender").value;
+  var [year, month, day] = date_value.split("-");
+  cc = year.substring(0, 2);
+  yy = year.toString().substr(-2);
 }
